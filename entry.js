f2c697239d5c70773652e2801e33485b5649b642
kithaca <kithaca@gmail.com>
2016-04-17 16:30:58 -0700

    initial svgsetup

---
+++ entry.js
@@ -0,0 +1,6 @@
+var svg = document.getElementById("svg");
+var width = window.innerWidth * 0.8;
+var height = window.innerHeight * 0.8;
+
+svg.setAttribute("height", height);
+svg.setAttribute("width", width);
